1abacae4b4049d70ee0b4166ee2532252455e37b
Anas <anaswail246@gmail.com>
2024-07-28 22:09:14 +0300

    v.1

--- main.js
+++ main.js
@@ -5,3 +5,30 @@ const menu = document.querySelector('.menu').addEventListener("click", ()=>{
     document.querySelector('nav').classList.toggle("-left-24");
     document.querySelector('.menu').classList.toggle('fa-angle-left')
 })
+
+// window onscroll
+
+window.onscroll = ()=>{
+    if(scrollY < 600){
+        document.querySelector('.left-nav').classList.add("remove")
+    }
+    else if(scrollY >= 600){
+        document.querySelector('.left-nav').classList.remove("remove");
+        if(scrollY >= 600 && scrollY < 1200) {
+            document.querySelector('.fa-circle-info').classList.add('text-[#b6b5ca]');
+        }
+        else{
+            document.querySelector('.fa-circle-info').classList.remove('text-[#b6b5ca]');
+            
+        }
+    }
+    
+    console.log(scrollY)
+}
+if(scrollY < 600){
+    document.querySelector('.left-nav').classList.add("remove")
+}
+else if(scrollY >= 600){
+    document.querySelector('.left-nav').classList.remove("remove")
+
+}
