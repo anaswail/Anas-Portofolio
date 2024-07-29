20f76920ffc79b572ced47b54f4310807b654beb
Anas <anaswail246@gmail.com>
2024-07-29 22:33:46 +0300

    v.1

--- main.js
+++ main.js
@@ -66,11 +66,16 @@ window.onscroll = () => {
     }
     if(scrollY >= 1000 && scrollY < 2000){
       document.querySelector(".fa-icons").classList.add("text-blue-600");
+    }
+    else{
+      document.querySelector(".fa-icons").classList.remove("text-blue-600");
+    }
+
+    if(scrollY >= 2800 && scrollY < 4200){
       document.querySelectorAll(".navItem")[3].classList.add("active");
     }
     else{
       document.querySelector(".fa-icons").classList.remove("text-blue-600");
-      document.querySelectorAll(".navItem")[3].classList.remove("active");
     }
 
     if(scrollY >= 2000 && scrollY < 3200){
